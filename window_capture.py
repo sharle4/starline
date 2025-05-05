4430a5091d65cbbe1851577e6deee08af1872617
Charles Kayssieh <charles.kayssieh@telecom-paris.fr>
2025-05-05 11:42:10 +0200

    change debug frame to frame in detection function args

--- window_capture.py
+++ window_capture.py
@@ -341,7 +341,7 @@ def find_aimcircle(frame, puck, arrow_start, arrow_end):
         print(f"distance au palet: {distance_points((best_circle[0], best_circle[1]), (x, y))}")
         
     if best_circle:
-        print(f"Cercle de visée trouvé : ({best_circle[0]}, {best_circle[1]}), R={best_circle[2]}")
+        #print(f"Cercle de visée trouvé : ({best_circle[0]}, {best_circle[1]}), R={best_circle[2]}")
         return aimcircle_tracker.smooth(best_circle)
     print("Aucun cercle de visée trouvé.")
     return aimcircle_tracker.smooth(None)
@@ -487,9 +487,11 @@ if __name__ == "__main__":
                 if puck:
                     x, y, r = puck
                     cv2.circle(debug_frame, (x, y), r, (0, 255, 0), 4)
-                    trajectory_start, trajectory_end = find_arrow_direction(debug_frame, puck)
-                    aim_circle = find_aimcircle(debug_frame, puck, trajectory_start, trajectory_end)
-                    if aim_circle: cv2.circle(debug_frame, (aim_circle[0], aim_circle[1]), aim_circle[2], (255, 0, 255), 2)
+                    trajectory_start, trajectory_end = find_arrow_direction(frame, puck)
+                    aim_circle = find_aimcircle(frame, puck, trajectory_start, trajectory_end)
+                    if aim_circle:
+                        print(f"Cercle de visée trouvé V222222222: ({aim_circle[0]}, {aim_circle[1]}), R={aim_circle[2]}")
+                        cv2.circle(debug_frame, (aim_circle[0], aim_circle[1]), 10, (255, 0, 255), 4)
                     width = monitor_info["width"]
                     height = monitor_info["height"]
                     
